7ca80e5038b8fd3ab710277bbdaee6cce5c290ed
Saccharide <rtang@email.wm.edu>
2018-11-18 13:41:41 -0500

    Added some new cool bash commands

--- cool-commands.sh
+++ cool-commands.sh
@@ -3,7 +3,7 @@
 # find all mp3 files in current directory and create a playlist of it
 find . -name "*.mp3" -exec echo "play" {} \; > playlist
 
-# Shuffles the playlist
+# Shuffles the lines of a file -R = random
 sort -R playlist -o playlist
 
 # Convert Chinese unreadable filename to correct filename (AWESOME)
@@ -11,3 +11,10 @@ sort -R playlist -o playlist
 
 # Find a string in current directory (-w stand for whole word, -r recursive)
 grep -rnw . -e "STRING"
+
+# Insert a newline at a specific line with sed -i [--in-place]
+sed -i '/SOME_LINE/i \n' foo.txt 
+
+# Read all files in a directory line by line
+ls | while read f; do while read -r line; do echo $line; done < $f ; done
+
